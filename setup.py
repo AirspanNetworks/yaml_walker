8f49546c49fdad4f58472132e92fbfa5627024d7
DmitryO <doguz2509@gmail.com>
2020-04-29 22:54:15 +0300

    setup.py: fix OS classifier

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     long_description=long_description,
     install_requires=['PyYAML'],
     classifiers=[
-        'Classifier: Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
