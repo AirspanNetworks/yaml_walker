f4ca9b96ccf48ad65285e75c083d8b11f35665a3
DmitryO <doguz2509@gmail.com>
2020-04-29 17:04:53 +0300

    Adding legal documents for project publish

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ setup(
     version='1.0.0',
     packages=['yaml_walker', 'yaml_walker.unittests'],
     url='https://github.com/doguz2509/YamlWalker',
-    license='',
+    license='MIT',
     author='Dmitry Oguz',
     author_email='doguz2509@gmail.com',
     description='Data walker (in dot notation style) & filter (Xpath analog) for yaml look data structure',
-    install_requires=['PyYAML']
+    install_requires=['PyYAML'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
 )
